mholt/caddy-l4

commit d1d54b015e34010da10b2f919e8db710c57f7731

Refactor TLS matcher/handler, and connection recording
This introduces some vast simplifications in how connections are
handled (I got rid of recordableConn, making Connection itself
recordable instead). We also don't remember TLS ClientHellos
anymore.

It solves a couple problems.

1)  TLS within TLS. Before, we assumed there would only be one
layer of TLS. This is not always the case: sometimes TLS is tunneled
over TLS and you want to configure a matcher on the inner TLS
connection (after terminating TLS). So, we no longer remember the
decoded ClientHello info and reuse it.

2) Recording after TLS termination. TLS termination replaces the
underlying Conn, but the recordableConn was still on the first
or inner Conn, before outer TLS was terminated. This meant that
ciphertext was recorded if using a matcher after TLS termination,
instead of the decrypted plaintext. Now, recording is done on the
current Conn handle, not always the initial one.

I probably broke some use cases here, but, hopefully not? I know
that I got some new use cases working.

layer4/connection.go
@@ -25,13 +25,18 @@ import (
 )
 
 // Connection contains information about the connection as it
-// passes through various handlers.
+// passes through various handlers. It also has the capability
+// of recording and rewinding when necessary.
+//
+// A Connection can be used as a net.Conn because it embeds a
+// net.Conn; but when wrapping underlying connections, usually
+// you want to be careful to replace the embedded Conn, not
+// this entire Connection value.
 //
 // Connection structs are NOT safe for concurrent use.
 type Connection struct {
-	// The underlying connection; use this for any
-	// wrapping and I/O.
-	Conn net.Conn
+	// The underlying connection.
+	net.Conn
 
 	// The context for the connection.
 	Context context.Context
@@ -39,13 +44,6 @@ type Connection struct {
 	buf       *bytes.Buffer // stores recordings
 	bufReader io.Reader     // used to read buf so it doesn't discard bytes
 	recording bool
-}
-
-// recordableConn can record data read from an underlying
-// Conn using the associated Connection struct.
-type recordableConn struct {
-	net.Conn
-	cx *Connection
 
 	bytesRead, bytesWritten uint64
 }
@@ -54,25 +52,25 @@ type recordableConn struct {
 // deplete any associated buffer from the prior recording,
 // and once depleted (or if there isn't one), it continues
 // reading from the underlying connection.
-func (rc *recordableConn) Read(p []byte) (n int, err error) {
+func (cx *Connection) Read(p []byte) (n int, err error) {
 	// if there is a buffer we should read from, start
 	// with that; we only read from the underlying conn
 	// after the buffer has been "depleted"
-	if rc.cx.bufReader != nil {
-		n, err = rc.cx.bufReader.Read(p)
+	if cx.bufReader != nil {
+		n, err = cx.bufReader.Read(p)
 		if err == io.EOF {
-			rc.cx.bufReader = nil
-		} else if err != nil || n > 0 {
-			return
+			cx.bufReader = nil
+			err = nil
 		}
+		return
 	}
 
 	// buffer has been "depleted" so read from
 	// underlying connection
-	n, err = rc.Conn.Read(p)
-	rc.bytesRead += uint64(n)
+	n, err = cx.Conn.Read(p)
+	cx.bytesRead += uint64(n)
 
-	if !rc.cx.recording {
+	if !cx.recording {
 		return
 	}
 
@@ -80,7 +78,7 @@ func (rc *recordableConn) Read(p []byte) (n int, err error) {
 	// was read needs to be written to the buffer, even
 	// if there was an error
 	if n > 0 {
-		if nw, errw := rc.cx.buf.Write(p[:n]); errw != nil {
+		if nw, errw := cx.buf.Write(p[:n]); errw != nil {
 			return nw, errw
 		}
 	}
@@ -88,13 +86,13 @@ func (rc *recordableConn) Read(p []byte) (n int, err error) {
 	return
 }
 
-func (rc *recordableConn) Write(p []byte) (n int, err error) {
-	n, err = rc.Conn.Write(p)
-	rc.bytesWritten += uint64(n)
+func (cx *Connection) Write(p []byte) (n int, err error) {
+	n, err = cx.Conn.Write(p)
+	cx.bytesWritten += uint64(n)
 	return
 }
 
-// record starts recording the stream into rc.buf.
+// record starts recording the stream into cx.buf.
 func (cx *Connection) record() {
 	cx.recording = true
 }

layer4/server.go
@@ -101,14 +101,10 @@ func (s Server) handle(conn net.Conn) {
 	defer bufPool.Put(buf)
 
 	cx := &Connection{
+		Conn:    conn,
 		Context: ctx,
 		buf:     buf,
 	}
-	rc := &recordableConn{
-		Conn: conn,
-		cx:   cx,
-	}
-	cx.Conn = rc
 
 	start := time.Now()
 	err := s.compiledRoute.Handle(cx)
@@ -118,8 +114,8 @@ func (s Server) handle(conn net.Conn) {
 	}
 
 	s.logger.Debug("connection stats",
-		zap.Uint64("read", rc.bytesRead),
-		zap.Uint64("written", rc.bytesWritten),
+		zap.Uint64("read", cx.bytesRead),
+		zap.Uint64("written", cx.bytesWritten),
 		zap.Duration("duration", duration),
 	)
 }

modules/l4proxy/proxy.go
@@ -198,6 +198,7 @@ func (h *Handler) dialPeers(upstream *Upstream, repl *caddy.Replacer, down *laye
 			}
 			up, err = tls.Dial(p.address.Network, hostPort, tlsCfg)
 		}
+		h.logger.Debug("dial upstream", zap.String("address", hostPort), zap.Error(err))
 		if err != nil {
 			h.countFailure(p)
 			for _, conn := range upConns {
@@ -217,7 +218,7 @@ func (h *Handler) proxy(down *layer4.Connection, upConns []net.Conn) {
 	// every time we read from downstream, we write
 	// the same to each upstream; this is half of
 	// the proxy duplex
-	var downTee io.Reader = down.Conn
+	var downTee io.Reader = down
 	for _, up := range upConns {
 		downTee = io.TeeReader(downTee, up)
 	}
@@ -228,7 +229,7 @@ func (h *Handler) proxy(down *layer4.Connection, upConns []net.Conn) {
 
 	for _, up := range upConns {
 		go func(up net.Conn) {
-			_, err := io.Copy(down.Conn, up)
+			_, err := io.Copy(down, up)
 			if err != nil && atomic.LoadInt32(&done) == 0 {
 				h.logger.Error("upstream connection",
 					zap.String("local_address", up.LocalAddr().String()),

modules/l4tls/handler.go
@@ -21,6 +21,7 @@ import (
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/modules/caddytls"
 	"github.com/mholt/caddy-l4/layer4"
+	"go.uber.org/zap"
 )
 
 func init() {
@@ -33,6 +34,7 @@ type Handler struct {
 
 	config *tls.Config
 	ctx    caddy.Context
+	logger *zap.Logger
 }
 
 // CaddyModule returns the Caddy module information.
@@ -46,6 +48,7 @@ func (Handler) CaddyModule() caddy.ModuleInfo {
 // Provision sets up the module.
 func (t *Handler) Provision(ctx caddy.Context) error {
 	t.ctx = ctx
+	t.logger = ctx.Logger(t)
 
 	// ensure there is at least one policy, which will act as default
 	if len(t.ConnectionPolicies) == 0 {
@@ -65,29 +68,23 @@ func (t *Handler) Handle(cx *layer4.Connection, next layer4.Handler) error {
 	// get the TLS config to use for this connection
 	tlsCfg := t.ConnectionPolicies.TLSConfig(t.ctx)
 
-	// if no prior matcher or handler read the ClientHello
-	// yet, we'll prepare to do so
-	clientHello, haveClientHello := cx.GetVar("tls_client_hello").(ClientHelloInfo)
-	if !haveClientHello {
-		underlyingGetConfigForClient := tlsCfg.GetConfigForClient
-		tlsCfg.GetConfigForClient = func(hello *tls.ClientHelloInfo) (*tls.Config, error) {
-			clientHello.ClientHelloInfo = *hello
-			return underlyingGetConfigForClient(hello)
-		}
+	// capture the ClientHello info when the handshake is performed
+	var clientHello ClientHelloInfo
+	underlyingGetConfigForClient := tlsCfg.GetConfigForClient
+	tlsCfg.GetConfigForClient = func(hello *tls.ClientHelloInfo) (*tls.Config, error) {
+		clientHello.ClientHelloInfo = *hello
+		return underlyingGetConfigForClient(hello)
 	}
 
 	// terminate TLS by performing the handshake
 	tlsConn := tls.Server(cx.Conn, tlsCfg)
-	if !haveClientHello {
-		err := tlsConn.Handshake()
-		if err != nil {
-			return err
-		}
-		cx.SetVar("tls_client_hello", clientHello)
+	err := tlsConn.Handshake()
+	if err != nil {
+		return err
 	}
+	t.logger.Debug("terminated TLS", zap.String("server_name", clientHello.ServerName))
 
-	// now all future reads/writes will be
-	// decrypted/encrypted at this point
+	// now all future reads/writes will be decrypted/encrypted
 	cx.Conn = tlsConn
 
 	return next.Handle(cx)

modules/l4tls/matcher.go
@@ -22,6 +22,7 @@ import (
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/modules/caddytls"
 	"github.com/mholt/caddy-l4/layer4"
+	"go.uber.org/zap"
 )
 
 func init() {
@@ -35,6 +36,7 @@ type MatchTLS struct {
 	MatchersRaw caddy.ModuleMap `json:"-" caddy:"namespace=tls.handshake_match"`
 
 	matchers []caddytls.ConnectionMatcher
+	logger   *zap.Logger
 }
 
 // CaddyModule returns the Caddy module information.
@@ -57,6 +59,7 @@ func (m MatchTLS) MarshalJSON() ([]byte, error) {
 
 // Provision sets up the handler.
 func (m *MatchTLS) Provision(ctx caddy.Context) error {
+	m.logger = ctx.Logger(m)
 	mods, err := ctx.LoadModule(m, "MatchersRaw")
 	if err != nil {
 		return fmt.Errorf("loading TLS matchers: %v", err)
@@ -69,42 +72,35 @@ func (m *MatchTLS) Provision(ctx caddy.Context) error {
 
 // Match returns true if the connection is a TLS handshake.
 func (m MatchTLS) Match(cx *layer4.Connection) (bool, error) {
-	// TODO: do we need a more standardized way to amortize matchers? or at least to remember decoded results from previous matchers?
-	chi, ok := cx.GetVar("tls_client_hello").(ClientHelloInfo)
-	if !ok {
-		// read the header bytes
-		const recordHeaderLen = 5
-		hdr := make([]byte, recordHeaderLen)
-		_, err := io.ReadFull(cx.Conn, hdr)
-		if err != nil {
-			return false, err
-		}
-
-		const recordTypeHandshake = 0x16
-		if hdr[0] != recordTypeHandshake {
-			return false, nil
-		}
-
-		// get length of the ClientHello message and read it
-		length := int(uint16(hdr[3])<<8 | uint16(hdr[4])) // ignoring version in hdr[1:3] - like https://github.com/inetaf/tcpproxy/blob/master/sni.go#L170
-		rawHello := make([]byte, length)
-		_, err = io.ReadFull(cx.Conn, rawHello)
-		if err != nil {
-			return false, err
-		}
-
-		// parse the ClientHello and store it in the map
-		chi = parseRawClientHello(rawHello)
-		chi.Conn = cx.Conn
-
-		// remember this so future handlers can use it
-		cx.SetVar("tls_client_hello", chi)
-
-		// also add values to the replacer
-		repl := cx.Context.Value(layer4.ReplacerCtxKey).(*caddy.Replacer)
-		repl.Set("l4.tls.server_name", chi.ClientHelloInfo.ServerName)
-		repl.Set("l4.tls.version", chi.Version)
+	// read the header bytes
+	const recordHeaderLen = 5
+	hdr := make([]byte, recordHeaderLen)
+	_, err := io.ReadFull(cx, hdr)
+	if err != nil {
+		return false, err
 	}
+
+	const recordTypeHandshake = 0x16
+	if hdr[0] != recordTypeHandshake {
+		return false, nil
+	}
+
+	// get length of the ClientHello message and read it
+	length := int(uint16(hdr[3])<<8 | uint16(hdr[4])) // ignoring version in hdr[1:3] - like https://github.com/inetaf/tcpproxy/blob/master/sni.go#L170
+	rawHello := make([]byte, length)
+	_, err = io.ReadFull(cx, rawHello)
+	if err != nil {
+		return false, err
+	}
+
+	// parse the ClientHello and store it in the map
+	chi := parseRawClientHello(rawHello)
+	chi.Conn = cx
+
+	// also add values to the replacer
+	repl := cx.Context.Value(layer4.ReplacerCtxKey).(*caddy.Replacer)
+	repl.Set("l4.tls.server_name", chi.ClientHelloInfo.ServerName)
+	repl.Set("l4.tls.version", chi.Version)
 
 	for _, matcher := range m.matchers {
 		// TODO: even though we have more data than the standard lib's
@@ -117,6 +113,8 @@ func (m MatchTLS) Match(cx *layer4.Connection) (bool, error) {
 			return false, nil
 		}
 	}
+
+	m.logger.Debug("matched", zap.String("server_name", chi.ClientHelloInfo.ServerName))
 
 	return true, nil
 }

modules/l4ssh/matcher.go
@@ -40,7 +40,7 @@ func (MatchSSH) CaddyModule() caddy.ModuleInfo {
 // Match returns true if the connection looks like SSH.
 func (m MatchSSH) Match(cx *layer4.Connection) (bool, error) {
 	p := make([]byte, len(sshPrefix))
-	n, err := io.ReadFull(cx.Conn, p)
+	n, err := io.ReadFull(cx, p)
 	if err != nil || n < len(sshPrefix) {
 		return false, nil
 	}

layer4/app.go
@@ -70,7 +70,7 @@ func (a *App) Start() error {
 						return err
 					}
 					a.packetConns = append(a.packetConns, pc)
-					lnAddr = pc.LocalAddr().String()
+					lnAddr = pc.LocalAddr().Network() + "/" + pc.LocalAddr().String()
 					go s.servePacket(pc)
 				} else {
 					ln, err := caddy.Listen(addr.Network, addr.JoinHostPort(i))
@@ -81,8 +81,7 @@ func (a *App) Start() error {
 					lnAddr = ln.Addr().Network() + "/" + ln.Addr().String()
 					go s.serve(ln)
 				}
-				s.logger.Debug("listening "+addr.Network,
-					zap.String("address", lnAddr))
+				s.logger.Debug("listening", zap.String("address", lnAddr))
 			}
 		}
 	}

modules/l4echo/echo.go
@@ -38,7 +38,7 @@ func (Handler) CaddyModule() caddy.ModuleInfo {
 
 // Handle handles the connection.
 func (Handler) Handle(cx *layer4.Connection, _ layer4.Handler) error {
-	_, err := io.Copy(cx.Conn, cx.Conn)
+	_, err := io.Copy(cx, cx)
 	return err
 }
 

commit 5f9948fe76ae19009258465fd66da1390aa5ecce

Properly re-wrap connections (fix #18)
Our Connection structs cannot be wrapped within themselves because you can't nest blocking reads, which the kernel enforces: this results in hanging behavior.

This fixes the regressions introduced in d1d54b015e34010da10b2f919e8db710c57f7731 (which is the commit that made Signal proxies work). I verified that Signal proxies still work with these changes.

layer4/connection.go
@@ -24,6 +24,27 @@ import (
 	"github.com/caddyserver/caddy/v2"
 )
 
+// WrapConnection wraps an underlying connection into a layer4 connection that
+// supports recording and rewinding, as well as adding context with a replacer
+// and variable table. This function is intended for use at the start of a
+// connection handler chain where the underlying connection is not yet a layer4
+// Connection value.
+func WrapConnection(underlying net.Conn, buf *bytes.Buffer) *Connection {
+	repl := caddy.NewReplacer()
+	repl.Set("l4.conn.remote_addr", underlying.RemoteAddr())
+	repl.Set("l4.underlying.local_addr", underlying.LocalAddr())
+
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, VarsCtxKey, make(map[string]interface{}))
+	ctx = context.WithValue(ctx, ReplacerCtxKey, repl)
+
+	return &Connection{
+		Conn:    underlying,
+		Context: ctx,
+		buf:     buf,
+	}
+}
+
 // Connection contains information about the connection as it
 // passes through various handlers. It also has the capability
 // of recording and rewinding when necessary.
@@ -90,6 +111,18 @@ func (cx *Connection) Write(p []byte) (n int, err error) {
 	n, err = cx.Conn.Write(p)
 	cx.bytesWritten += uint64(n)
 	return
+}
+
+// Wrap wraps conn in a new Connection based on cx (reusing
+// cx's existing buffer and context). This is useful after
+// a connection is wrapped by a package that does not support
+// our Connection type (for example, `tls.Server()`).
+func (cx *Connection) Wrap(conn net.Conn) *Connection {
+	return &Connection{
+		Conn:    conn,
+		Context: cx.Context,
+		buf:     cx.buf,
+	}
 }
 
 // record starts recording the stream into cx.buf.

layer4/server.go
@@ -16,7 +16,6 @@ package layer4
 
 import (
 	"bytes"
-	"context"
 	"fmt"
 	"net"
 	"sync"
@@ -88,23 +87,11 @@ func (s Server) servePacket(pc net.PacketConn) error {
 func (s Server) handle(conn net.Conn) {
 	defer conn.Close()
 
-	repl := caddy.NewReplacer()
-	repl.Set("l4.conn.remote_addr", conn.RemoteAddr())
-	repl.Set("l4.conn.local_addr", conn.LocalAddr())
-
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, VarsCtxKey, make(map[string]interface{}))
-	ctx = context.WithValue(ctx, ReplacerCtxKey, repl)
-
 	buf := bufPool.Get().(*bytes.Buffer)
 	buf.Reset()
 	defer bufPool.Put(buf)
 
-	cx := &Connection{
-		Conn:    conn,
-		Context: ctx,
-		buf:     buf,
-	}
+	cx := WrapConnection(conn, buf)
 
 	start := time.Now()
 	err := s.compiledRoute.Handle(cx)
@@ -135,8 +122,6 @@ func (pc packetConn) Write(b []byte) (n int, err error) {
 }
 
 func (pc packetConn) RemoteAddr() net.Addr { return pc.addr }
-
-func (packetConn) Close() error { return nil }
 
 var udpBufPool = sync.Pool{
 	New: func() interface{} {

modules/l4proxy/proxy.go
@@ -192,8 +192,8 @@ func (h *Handler) dialPeers(upstream *Upstream, repl *caddy.Replacer, down *laye
 			tlsCfg := upstream.tlsConfig
 			if tlsCfg == nil {
 				tlsCfg = new(tls.Config)
-				if chi, ok := down.GetVar("tls_client_hello").(l4tls.ClientHelloInfo); ok {
-					chi.FillTLSClientConfig(tlsCfg)
+				if hellos := l4tls.GetClientHelloInfos(down); len(hellos) > 0 {
+					hellos[0].FillTLSClientConfig(tlsCfg)
 				}
 			}
 			up, err = tls.Dial(p.address.Network, hostPort, tlsCfg)

modules/l4tls/handler.go
@@ -32,7 +32,6 @@ func init() {
 type Handler struct {
 	ConnectionPolicies caddytls.ConnectionPolicies `json:"connection_policies,omitempty"`
 
-	config *tls.Config
 	ctx    caddy.Context
 	logger *zap.Logger
 }
@@ -76,18 +75,44 @@ func (t *Handler) Handle(cx *layer4.Connection, next layer4.Handler) error {
 		return underlyingGetConfigForClient(hello)
 	}
 
-	// terminate TLS by performing the handshake
-	tlsConn := tls.Server(cx.Conn, tlsCfg)
+	// terminate TLS by performing the handshake (note that we pass
+	// in cx, not cx.Conn; this is because we must read from the
+	// connection to perform the handshake, and cx might have some
+	// bytes already buffered need to be read first)
+	tlsConn := tls.Server(cx, tlsCfg)
 	err := tlsConn.Handshake()
 	if err != nil {
 		return err
 	}
 	t.logger.Debug("terminated TLS", zap.String("server_name", clientHello.ServerName))
 
-	// now all future reads/writes will be decrypted/encrypted
-	cx.Conn = tlsConn
+	// preserve this ClientHello info for later, if needed
+	appendClientHello(cx, clientHello)
 
-	return next.Handle(cx)
+	// all future reads/writes will now be decrypted/encrypted
+	// (tlsConn, which wraps cx, is wrapped into a new cx so
+	// that future I/O succeeds... if we use the same cx, it'd
+	// be wrapping itself, and we'd have nested read calls out
+	// to the kernel, which creates a deadlock/hang; see #18)
+	return next.Handle(cx.Wrap(tlsConn))
+}
+
+func appendClientHello(cx *layer4.Connection, chi ClientHelloInfo) {
+	var clientHellos []ClientHelloInfo
+	if val := cx.GetVar("tls_client_hellos"); val != nil {
+		clientHellos = val.([]ClientHelloInfo)
+	}
+	clientHellos = append(clientHellos, chi)
+	cx.SetVar("tls_client_hellos", clientHellos)
+}
+
+// GetClientHelloInfos gets ClientHello information for all the terminated TLS connections.
+func GetClientHelloInfos(cx *layer4.Connection) []ClientHelloInfo {
+	var clientHellos []ClientHelloInfo
+	if val := cx.GetVar("tls_client_hellos"); val != nil {
+		clientHellos = val.([]ClientHelloInfo)
+	}
+	return clientHellos
 }
 
 // Interface guards

modules/l4tls/matcher.go
@@ -93,7 +93,7 @@ func (m MatchTLS) Match(cx *layer4.Connection) (bool, error) {
 		return false, err
 	}
 
-	// parse the ClientHello and store it in the map
+	// parse the ClientHello
 	chi := parseRawClientHello(rawHello)
 	chi.Conn = cx
 