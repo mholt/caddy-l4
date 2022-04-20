aec6535658b117e6283cc2bdaa80d17462fd2eef
ydylla <ydylla@gmail.com>
2022-04-20 19:46:01 +0200

    proxyprotocol: Rewrite proxy protocol matcher (#58)
    * refactor: rewrite proxy matcher
    
    Do not use a bufio.Reader to make the fix for #55 more reliable
    
    * test: add tests for proxy protocol handler
    
    * update dependencies
    
    * test: remove unsafe config injection
    
    Not needed anymore with up to date caddy version
    
    * refactor: remove constants

---
+++ modules/l4proxyprotocol/handler_test.go
@@ -0,0 +1,132 @@
+package l4proxyprotocol
+
+import (
+	"bytes"
+	"context"
+	"github.com/caddyserver/caddy/v2"
+	"github.com/mholt/caddy-l4/layer4"
+	"io"
+	"net"
+	"sync"
+	"testing"
+)
+
+func assertString(t *testing.T, expected string, value string) {
+	t.Helper()
+	if value != expected {
+		t.Fatalf("Expected '%s' but got '%s'\n", expected, value)
+	}
+}
+
+func TestProxyProtocolHandleV1(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	in, out := net.Pipe()
+	defer closePipe(wg, in, out)
+
+	cx := layer4.WrapConnection(in, &bytes.Buffer{})
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		defer out.Close()
+		_, err := out.Write(ProxyV1Example)
+		assertNoError(t, err)
+	}()
+
+	ctx, cancel := caddy.NewContext(caddy.Context{Context: context.Background()})
+	defer cancel()
+
+	handler := Handler{}
+	err := handler.Provision(ctx)
+	assertNoError(t, err)
+
+	var nextCx *layer4.Connection
+	err = handler.Handle(cx, layer4.HandlerFunc(func(c *layer4.Connection) error {
+		nextCx = c
+		return nil
+	}))
+	assertNoError(t, err)
+
+	if nextCx == nil {
+		t.Fatalf("handler did not call next")
+	}
+
+	assertString(t, "192.168.0.1:56324", nextCx.RemoteAddr().String())
+	assertString(t, "192.168.0.11:443", nextCx.LocalAddr().String())
+
+	_, _ = io.Copy(io.Discard, in)
+}
+
+func TestProxyProtocolHandleV2(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	in, out := net.Pipe()
+	defer closePipe(wg, in, out)
+
+	cx := layer4.WrapConnection(in, &bytes.Buffer{})
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		defer out.Close()
+		_, err := out.Write(ProxyV2Example)
+		assertNoError(t, err)
+	}()
+
+	ctx, cancel := caddy.NewContext(caddy.Context{Context: context.Background()})
+	defer cancel()
+
+	handler := Handler{}
+	err := handler.Provision(ctx)
+	assertNoError(t, err)
+
+	var nextCx *layer4.Connection
+	err = handler.Handle(cx, layer4.HandlerFunc(func(c *layer4.Connection) error {
+		nextCx = c
+		return nil
+	}))
+	assertNoError(t, err)
+
+	if nextCx == nil {
+		t.Fatalf("handler did not call next")
+	}
+
+	assertString(t, "127.0.0.1:47111", nextCx.RemoteAddr().String())
+	assertString(t, "127.0.0.1:443", nextCx.LocalAddr().String())
+
+	_, _ = io.Copy(io.Discard, in)
+}
+
+func TestProxyProtocolHandleGarbage(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	in, out := net.Pipe()
+	defer closePipe(wg, in, out)
+
+	cx := layer4.WrapConnection(in, &bytes.Buffer{})
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		defer out.Close()
+		_, err := out.Write([]byte("some garbage"))
+		assertNoError(t, err)
+	}()
+
+	ctx, cancel := caddy.NewContext(caddy.Context{Context: context.Background()})
+	defer cancel()
+
+	handler := Handler{}
+	err := handler.Provision(ctx)
+	assertNoError(t, err)
+
+	var nextCx *layer4.Connection
+	err = handler.Handle(cx, layer4.HandlerFunc(func(c *layer4.Connection) error {
+		nextCx = c
+		return nil
+	}))
+	if err == nil || err.Error() != "parsing the PROXY header: invalid signature" {
+		t.Fatalf("handler did not return an error or the wrong error -> %s", err)
+	}
+
+	if nextCx != nil {
+		t.Fatalf("handler did call next")
+	}
+
+	_, _ = io.Copy(io.Discard, in)
+}
--- modules/l4proxyprotocol/matcher.go
+++ modules/l4proxyprotocol/matcher.go
@@ -12,12 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package l4proxy
+package l4proxyprotocol
 
 import (
-	"bufio"
 	"bytes"
-	"errors"
 	"io"
 
 	"github.com/caddyserver/caddy/v2"
@@ -26,7 +24,7 @@ import (
 
 // https://www.haproxy.org/download/1.8/doc/proxy-protocol.txt
 var (
-	headerV1Prefix = []byte("PROX") // intentional to not include "Y", see match() function for details
+	headerV1Prefix = []byte("PROXY")
 	headerV2Prefix = []byte{0x0D, 0x0A, 0x0D, 0x0A, 0x00, 0x0D, 0x0A, 0x51, 0x55, 0x49, 0x54, 0x0A}
 )
 
@@ -46,39 +44,15 @@ func (MatchProxyProtocol) CaddyModule() caddy.ModuleInfo {
 
 // Match returns true if the connection looks like it is using the Proxy Protocol.
 func (m MatchProxyProtocol) Match(cx *layer4.Connection) (bool, error) {
-	p := make([]byte, 4)
-	_, err := io.ReadFull(cx, p)
+	buf := make([]byte, len(headerV2Prefix))
+	_, err := io.ReadFull(cx, buf)
 	if err != nil {
 		return false, err
 	}
-	if bytes.Equal(p, headerV1Prefix) {
+
+	if bytes.HasPrefix(buf, headerV1Prefix) {
 		return true, nil
 	}
-
-	buf := p[:]
-	bufReader := bufio.NewReader(cx)
-	for i := 1; i <= 8; i++ {
-		// read the next 8 bytes, one byte at a time
-		// since 5th byte being null is valid in v2 header
-		// but is considered EOF by readers
-		b, err := bufReader.ReadByte()
-		if err == nil {
-			buf = append(buf, b)
-			continue
-		}
-
-		if !errors.Is(err, io.ErrUnexpectedEOF) {
-			return false, err
-		}
-
-		// 4 bytes were already read, so i == 1 is the 5th byte
-		if i != 1 {
-			return false, err
-		}
-
-		buf = append(buf, 0x00)
-	}
-
 	if bytes.Equal(buf, headerV2Prefix) {
 		return true, nil
 	}
