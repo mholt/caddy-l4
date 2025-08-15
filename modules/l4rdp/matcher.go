38d05e630ba2b1144fddb4b93d8253b8ee081523
vnxme <46669194+vnxme@users.noreply.github.com>
2025-08-15 22:12:12 +0300

    refactor: Format imports

--- modules/l4rdp/matcher.go
+++ modules/l4rdp/matcher.go
@@ -29,6 +29,7 @@ import (
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+
 	"github.com/mholt/caddy-l4/layer4"
 )
 
