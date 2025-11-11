1823cde2603c124d48b7843729747f66e941fa2f
a <a@tuxpa.in>
2025-11-10 22:21:33 -0600

    fix ci, gosec false positive. bump deps (#343)
    * noot
    
    * yessir
    
    * oops
    
    * noot
    
    * noot
    
    * bump deps

--- modules/l4tls/matcher.go
+++ modules/l4tls/matcher.go
@@ -88,6 +88,7 @@ func (m *MatchTLS) Match(cx *layer4.Connection) (bool, error) {
 	}
 
 	// get length of the ClientHello message and read it
+	// nolint:gosec // disable G602 // https://github.com/securego/gosec/issues/1406
 	length := int(uint16(hdr[3])<<8 | uint16(hdr[4])) // ignoring version in hdr[1:3] - like https://github.com/inetaf/tcpproxy/blob/master/sni.go#L170
 	rawHello := make([]byte, length)
 	_, err = io.ReadFull(cx, rawHello)
